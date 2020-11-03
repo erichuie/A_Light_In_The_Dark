1563b80e05ecfe2ec3473da2c44ce7bc6a080871
Eric <eric.huie@mail.mcgill.ca>
2020-11-03 15:11:41 -0800

    First commit. Issues are with making a transition between first and second scene and making a sprint jump.

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Camera mainCam;
+
+    public GameObject player;
+    public GameObject triggerToScene2;
+    public GameObject triggerToScene1;
+
+    void OnTriggerEnter2D(Collider2D collision) {
+         if (collision.tag == "enterToScene2") {
+            triggerToScene1.SetActive(false);
+            mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y + 16, mainCam.transform.position.z);
+            triggerToScene2.SetActive(false);
+            StartCoroutine("CoroutineTest1");
+
+         }
+         else if(collision.tag == "enterToScene1") {
+            triggerToScene2.SetActive(false);
+            mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y - 16, mainCam.transform.position.z);
+            triggerToScene1.SetActive(false);
+            StartCoroutine("CoroutineTest2");
+
+         }
+    }
+
+    IEnumerator CoroutineTest1() {
+             yield return new WaitForSeconds((float)0.5);
+            triggerToScene1.SetActive(true);
+            //  mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y + 15, mainCam.transform.position.z);
+         } 
+    IEnumerator CoroutineTest2() {
+            yield return new WaitForSeconds((float)0.5);
+        triggerToScene2.SetActive(true);
+        //  mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y + 15, mainCam.transform.position.z);
+        } 
+}
---
+++ Assets/Scripts/PlayerMovementLite.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovementLite : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpSpeed;
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+    public float groundedMemory; 
+    float prevTimeGrounded;
+    public float fallMultiplier = 2.5f; 
+    public float lowJumpMultiplier = 10f;
+
+    public float horizontalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("begin");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //horizontal and jump movement
+        float x = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(x * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - 
+        prevTimeGrounded <= groundedMemory))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+         BetterJump();
+        CheckIfGrounded();
+        Sprint();
+    }
+
+    void FixedUpdate(){
+
+    }
+
+    void Sprint(){
+        if (Input.GetKeyDown(KeyCode.LeftShift)){
+            print("works");
+            float x = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(x * speed, rb.velocity.y);
+        }
+    }
+
+    void BetterJump(){
+    if (rb.velocity.y < 0) {
+        rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+    } else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) {
+        rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+    }   
+    }
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            if(isGrounded){
+                prevTimeGrounded = Time.time;
+            }
+            isGrounded = false;
+        }
+    }
+}
