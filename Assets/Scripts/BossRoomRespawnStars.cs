ea8e7df4b9d467bd59227556b2db8aa0c3583ae6
Eric Huie <erich7789@gmail.com>
2020-11-13 16:42:32 -0800

    Added a sort of boss level. Has bug where respawning of green stars is continuous

--- Assets/Scripts/BossRoomRespawnStars.cs
+++ Assets/Scripts/BossRoomRespawnStars.cs
@@ -6,23 +6,29 @@ public class BossRoomRespawnStars : MonoBehaviour
 {
 
     private float respawnTimer = 0;
-    public float delayTime = 1000;
+    public float delayTime;
+    
 
-
-    void Update(){
-        print(respawnTimer);
-        respawnTimer += 1;
-        if(gameObject.active == false && respawnTimer > delayTime){
-            Instantiate(gameObject, transform.position,
-        transform.rotation);
-            gameObject.SetActive(true);
-        }
-    }
+    // void Update(){
+    //    // print(respawnTimer);
+    //     respawnTimer += Time.deltaTime;
+    //     if(respawnTimer > delayTime){
+    //         gameObject.SetActive(true);
+    //     }
+    // }
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player"){
             gameObject.SetActive(false);
-            respawnTimer = 0;
+            //gameObject.SetActive(true);
+           //respawnTimer = 0;
+            // StartCoroutine("RespawnStar");
         }
     }
+
+    // IEnumerator RespawnStar(){
+    //     print("respawning");
+    //     yield return new WaitForSeconds(3);
+    //     gameObject.SetActive(true);
+    // }
 }
---
+++ Assets/Scripts/GreenStarController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenStarController : MonoBehaviour
+{
+
+    public GameObject[] greenStars;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for(int i=0; i< greenStars.Length; i++){
+            if (greenStars[i].active == false){
+                StartCoroutine("RespawnStar", greenStars[i]);
+            }
+        }
+    }
+
+    IEnumerator RespawnStar(GameObject star){
+        yield return new WaitForSeconds(6);
+        star.SetActive(true);
+    }
+}
